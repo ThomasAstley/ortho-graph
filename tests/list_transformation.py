12d2890bf657dba61d10f0533f3285acd0b64a00
Tom Astley <astleytj2@gmail.com>
2023-11-02 10:37:55 +0100

    CHANGED: list edges in sets

--- tests/list_transformation.py
+++ tests/list_transformation.py
@@ -33,33 +33,30 @@ class TestTransformInputGraph(unittest.TestCase):
         Test list of nodes and edges for each graph 
         """
 
-        file_path = 'examples/two_graphs'
+        file_path = 'tests/list_transformation_graph_description_2'
         
         graphs = parse_graph_descriptions(file_path)
-        # pprint(graphs)
 
         first_graph = graphs['Graph']
-        first_nodes = first_graph['nodes']
-        first_edges = first_graph['edges']
+
+        first_expected_graph = {
+                            'nodes': {'A', 'B', 'C', 'D'},
+                            'edges': [{'A': 'B'}, {'B': 'C'}, {'B': 'D'}, {'C': 'D'}]
+                            }
+
+        self.assertEqual(first_graph, first_expected_graph)
         
-        first_expected_nodes = {'A', 'B', 'C', 'D'}
-        first_expected_edges = [{'A': 'B'}, {'B': 'C'}, {'B': 'D'}, {'C': 'D'}]
-
-        self.assertEqual(first_nodes, first_expected_nodes)
-        self.assertEqual(first_edges, first_expected_edges)
-
         # ------------------------------------
 
         second_graph = graphs['A']
-        second_nodes = second_graph['nodes']
-        second_edges = second_graph['edges']
+
+        second_expected_graph = {
+                            'nodes': {1, 'a', 2, 'b'},
+                            'edges': [{1: 'a'}, {2: 'b'}]
+                            }
+
+        self.assertEqual(second_graph, second_expected_graph)
         
-        second_expected_nodes = {1, 'a', 2, 'b'}
-        second_expected_edges = [{1: 'a'}, {2: 'b'}]
-
-        self.assertEqual(second_nodes, second_expected_nodes)
-        self.assertEqual(second_edges, second_expected_edges)
-
 #complicated graphs
 
 if __name__ == '__main__':
